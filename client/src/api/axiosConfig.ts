12afe4def347d4238c8615c8f0f3070ef5c520ee
corey clarke <sombas100@yahoo.com>
2025-02-19 23:32:17 +0000

    changes

--- client/src/api/axiosConfig.ts
+++ client/src/api/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = import.meta.env.VITE_API_URL;
 
 export const client = axios.create({
-    baseURL: API,
+    baseURL: API || 'https://easylearn-backend-ufbk.onrender.com/api',
     headers: {
         "Content-Type": "application/json"
     },
