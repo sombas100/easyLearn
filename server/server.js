234d986b3dbc7aace8544116dbded7fe90fc2726
corey clarke <sombas100@yahoo.com>
2025-02-19 01:46:16 +0000

    changes to host

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const lessonRoutes = require('./routes/lessonRoutes');
 const enrollmentRoutes = require('./routes/enrollmentRoutes');
 const statRoutes = require('./routes/statRoutes')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(cors());
